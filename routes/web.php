458943753148e2ac6775442f13035ebdf74d02aa
Clarice <clariceting2006@gmail.com>
2025-10-12 23:57:31 +0700

    moved logic from route to controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\MealController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Meal;
 
@@ -13,28 +14,8 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::get('/menu', function () {
-    $query = Meal::where('isAvailable', true);
-    
-    // Filter by category if provided
-    if (request('category')) {
-        $query->where('category', request('category'));
-    }
-    
-    $meals = $query->paginate(12); // 12 items per page
-    return view('menu.index', compact('meals'));
-})->name('menu');
-
-// Route untuk detail meal - pakai menu.show
-Route::get('/menu/{id}', function ($id) {
-    $meal = Meal::with('reviews.user')->findOrFail($id);
-    $suggestedMeals = Meal::where('isAvailable', true)
-                            ->where('id', '!=', $id)
-                            ->inRandomOrder()
-                            ->take(4)
-                            ->get();
-    return view('menu.show', compact('meal', 'suggestedMeals'));
-})->name('menu.show');
+Route::get('/menu', [MealController::class, 'index'])->name('menu');
+Route::get('/menu/{id}', [MealController::class, 'show'])->name('menu.show');
 
 
 Route::get('/cart', [CartController::class, 'cart']);
