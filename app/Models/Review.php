f49dc2a1939e7f2e5077f4211bae8e22e6f44ead
felix <ffelixrichardo@gmail.com>
2025-10-12 23:48:14 +0700

    fix error in review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -20,14 +20,4 @@ class Review extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'meal_id', 'message'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function meal()
-    {
-        return $this->belongsTo(Meal::class);
-    }
 }
