2a0885e692d0bf2f15a657cd17e7b14cfa8a3b94
felix <ffelixrichardo@gmail.com>
2025-10-14 01:40:31 +0700

    update hero bg compress, and update route so it use controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Meal;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+    public function index(){
+        $meals = Meal::where('isAvailable', true)->take(6)->get();
+        return view('home', compact('meals'));
+    }
+
     public function about(){
         return view('about');
     }
 
-    
 }
