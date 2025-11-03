85f1d6fc18289704b63c0f85bf5814f96a9623ff
RiceUC <clarice.ciputra@gmail.com>
2025-11-04 01:11:27 +0700

    Search & Meals CRUD

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Storage;
 
+use App\Models\Meal;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
 
     public function dashboardMeal(){
-        return view('dashboard.menuDashboard');
+        $meals = Meal::orderBy('created_at', 'desc')->get();
+        return view('dashboard.menuDashboard', compact('meals'));
     }
 
     function dashboardUser(){
